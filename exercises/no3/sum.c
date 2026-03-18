c773e53f878be9ca5c93de1b8bfe512b0cb4c6c9
KhaledLemes <khaled.borges@outlook.com>
2026-03-18 19:38:15 -0300

    minor correction

--- exercises/no3/sum.c
+++ exercises/no3/sum.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main() {
-    // Sum up each element of an array
+    // Sum up each element of an matrix
     int mat[3][3];
     int sum = 0;
     srand (time(NULL));
@@ -12,8 +12,8 @@ int main() {
     for (int j = 0; j < 3; j++) {
         printf("\t");
         for (int k = 0; k < 3; k++) {
-            int val = mat[j][k] = 100 + (rand() % 999);
-            sum += val;
+            // int val = mat[j][k] = 100 + (rand() % 999); <--- This var was just used for debugging
+            sum += mat[j][k] = 100 + (rand() % 999);
             printf("%d ", mat[j][k]);
         }
         printf("\n");
