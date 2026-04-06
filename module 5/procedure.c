7edc446bb6641546f943932ea95985ef29d4afaa
KhaledLemes <khaled.borges@outlook.com>
2026-04-06 19:35:45 -0300

    functions and procedures

--- module 5/procedure.c
+++ module 5/procedure.c
@@ -1,3 +1,18 @@
-//
-// Created by khal on 06/04/2026.
-//
+#include <stdio.h>
+
+// funcs that won't return nothing
+void printsmt(char *str, int amount_of_times) {
+    for (int i = 0; i < amount_of_times; i++) {
+        printf("%s", str);
+    }
+}
+
+int main() {
+    int amt = 0;
+    char str[101];
+    printf("Type something: ");
+    fgets(str, 101,stdin);
+    printf("Now the amount of times to print it: ");
+    scanf("%d", &amt);
+    printsmt(str, amt);
+}
