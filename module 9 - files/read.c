888c8b77caebaf8fdcbce09d73395c69be049fc7
KhaledLemes <khaled.borges@outlook.com>
2026-04-20 19:54:37 -0300

    opening, reading and writing files - reading files

--- module 9 - files/read.c
+++ module 9 - files/read.c
@@ -1,3 +1,15 @@
-//
-// Created by khal on 20/04/2026.
-//
+#include <stdio.h>
+
+int main() {
+    FILE *f = fopen("test.txt", "r");
+    char str[100];
+
+    if (f) {
+        fgets(str, 100, f);
+        printf("%s", str);
+        fclose(f);
+    }
+    else
+        printf("coding isn't really for u man");
+    return 0;
+}
