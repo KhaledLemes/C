5f3861a98d5d612db6f48926ed52a706a00e08af
KhaledLemes <khaled.borges@outlook.com>
2026-05-20 13:54:23 -0300

    Abstract Data Type - ADT | remove item

--- Module 10 - Dynamic Data Structures/lines/headers/line.h
+++ Module 10 - Dynamic Data Structures/lines/headers/line.h
@@ -1,8 +1,17 @@
-//
-// Created by khal on 10/05/2026.
-//
-
 #ifndef LEARNING_LINE_H
 #define LEARNING_LINE_H
+
+typedef struct node {
+    int n;
+    struct node *next;
+}Node;
+
+typedef struct {
+    Node *start, *end;
+}Line;
+
+Line* create_line();
+int insert_to_line(Line *line, int n);
+Node *remove_last_item(Line *line);
 
 #endif //LEARNING_LINE_H
