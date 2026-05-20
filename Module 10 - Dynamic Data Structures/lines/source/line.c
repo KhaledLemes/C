f40be1e9b047f3bcce12366ad640c5400bcd84b9
KhaledLemes <khaled.borges@outlook.com>
2026-05-20 13:53:15 -0300

    Abstract Data Type - ADT | remove item

--- Module 10 - Dynamic Data Structures/lines/source/line.c
+++ Module 10 - Dynamic Data Structures/lines/source/line.c
@@ -1,3 +1,33 @@
-//
-// Created by khal on 10/05/2026.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/line.h"
+
+Line *create_line() {
+    Line *line = malloc(sizeof(Line));
+    line->start = NULL;
+    line->end = NULL;
+    return line;
+}
+
+int insert_to_line(Line *line, int n) {
+    Node *new = malloc(sizeof(Node));
+    if (new) {
+        new->n = n;
+        new->next = NULL;
+        if (line->end) {
+            line->end->next = new;
+            line->end = new;
+        } else {
+            line->start = new;
+            line->end = new;
+        }
+        return 0;
+    }
+        return -1;
+}
+
+Node *remove_last_item(Line *line) {
+    Node *removed = line->start;
+    line->start = line->start->next;
+    return removed;
+}
--- Module 10 - Dynamic Data Structures/lines/source/main.c
+++ Module 10 - Dynamic Data Structures/lines/source/main.c
@@ -1,3 +1,25 @@
-//
-// Created by khal on 10/05/2026.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/line.h"
+
+int main() {
+    Line *line = create_line();
+    for (int i = 0; i < 8; i++) {
+        int err = insert_to_line(line, i+1);
+        if (err != 0) {
+            printf("Error allocating memory");
+            return err;
+        }
+        printf("Beggining: %d\nEnd:%d\n", line->start->n, line->end->n);
+    }
+
+    Node *removed = remove_last_item(line);
+    printf("Removed (before loop): %d\nBeggining: %d\nEnd:%d\n\n", removed->n,line->start->n, line->end->n);
+    free(removed);
+    for (int i = 0; i < 7; i++) {
+        removed = remove_last_item(line);
+        printf("Removed: %d\n", removed->n);
+        free(removed);
+    }
+    free(line);
+}
