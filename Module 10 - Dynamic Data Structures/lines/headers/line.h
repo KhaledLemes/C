bc9f99b223e375fc234fe58744666467dca17f50
KhaledLemes <khaled.borges@outlook.com>
2026-05-28 23:00:33 -0300

    Priority

--- Module 10 - Dynamic Data Structures/lines/headers/line.h
+++ Module 10 - Dynamic Data Structures/lines/headers/line.h
@@ -12,6 +12,8 @@ typedef struct {
 
 Line* create_line();
 int insert_to_line(Line *line, int n);
+void priority_insert(Line *line, Node *new);
+void non_priority_insert(Line *line, Node *new);
 Node *remove_last_item(Line *line);
 
 #endif //LEARNING_LINE_H
--- Module 10 - Dynamic Data Structures/lines/source/line.c
+++ Module 10 - Dynamic Data Structures/lines/source/line.c
@@ -9,21 +9,44 @@ Line *create_line() {
     return line;
 }
 
+void priority_insert(Line *line, Node *new) {
+    if (line->start->n < 60) {
+        new->next = line->start;
+        line->start = new;
+    } else {
+        Node *aux = line->start;
+        while (aux->next && aux->next->n >= 60) {
+            aux = aux->next;
+        }
+        new->next = aux->next;
+        aux->next = new;
+    }
+}
+
+void non_priority_insert(Line *line, Node *new) {
+    if (line->end) {
+        line->end->next = new;
+        line->end = new;
+    } else {
+        line->start = new;
+        line->end = new;
+    }
+}
+
 int insert_to_line(Line *line, int n) {
     Node *new = malloc(sizeof(Node));
     if (new) {
         new->n = n;
         new->next = NULL;
-        if (line->end) {
-            line->end->next = new;
-            line->end = new;
-        } else {
-            line->start = new;
-            line->end = new;
+        if (n < 60) {
+            // Priority is numbers above 59
+            non_priority_insert(line, new);
+            return 0;
         }
+        priority_insert(line, new);
         return 0;
     }
-        return -1;
+    return -1;
 }
 
 Node *remove_last_item(Line *line) {
